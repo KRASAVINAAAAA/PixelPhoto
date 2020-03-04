6a8ca4ed869355c884b6051040131ba0667acdb1
KRASAVINAAAAA <60936598+KRASAVINAAAAA@users.noreply.github.com>
2020-03-05 02:35:27 +0300

    Пользовательский интерфейс создан Добавлены: pictureBox,  контейнер(2части Split), трекбар и открытие файла(ToolStrip)

---
+++ photoPIXEL/photoPIXEL/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace photoPIXEL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+            }
+        }
+    }
+}
